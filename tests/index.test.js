4ad850e81c503801b14c076835840fe7f4c079dc
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-11-06 23:18:15 +0530

    test cases for user avatar information

--- tests/index.test.js
+++ tests/index.test.js
@@ -63,7 +63,7 @@ describe("Authentication", () => {
 
 describe("User meta data endpoints", () => {
   let token = "";
-  let avatarId="";
+  let avatarId = "";
   beforeAll(async () => {
     const username = `Pratik-${Math.random()}`;
     const password = "1234";
@@ -78,40 +78,90 @@ describe("User meta data endpoints", () => {
     });
     token = response.data.token;
 
-    const avatarResponse = await axios.post(`${BACKEND_URL}/api/v1/admin/avatar`,{
-        "imageUrl" : "https://ideogram.ai/assets/progressive-image/balanced/response/Bfvkdk12Soq7_mlr3PXJUg",
-        "name" : "Hercules"
-    })
+    const avatarResponse = await axios.post(
+      `${BACKEND_URL}/api/v1/admin/avatar`,
+      {
+        imageUrl:
+          "https://ideogram.ai/assets/progressive-image/balanced/response/Bfvkdk12Soq7_mlr3PXJUg",
+        name: "Hercules",
+      }
+    );
     avatarId = avatarResponse.data.avatarId;
   });
 
-  test("User can't update their metadata with wrong avatar id",async()=>{
-    const response = await axios.post(`${BACKEND_URL}/api/v1/user/metadata`, {
-        avatarId : "123"
-      },{
-        headers:{
-            "authorization" : `Bearer ${token}`
-        }
-      });
+  test("User can't update their metadata with wrong avatar id", async () => {
+    const response = await axios.post(
+      `${BACKEND_URL}/api/v1/user/metadata`,
+      {
+        avatarId: "123",
+      },
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
 
-      expect(response.statusCode).toBe(400)
-  })
+    expect(response.statusCode).toBe(400);
+  });
 
-  test("User can update their metadata with the right avatar id",async()=>{
-    const response = await axios.post(`${BACKEND_URL}/api/v1/user/metadata`,{
-        avatarId
-    },{
-        headers:{
-            "authorization" : `Bearer ${token}`
-        }
-    })
+  test("User can update their metadata with the right avatar id", async () => {
+    const response = await axios.post(
+      `${BACKEND_URL}/api/v1/user/metadata`,
+      {
+        avatarId,
+      },
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
     expect(response.statusCode).toBe(200);
-  })
-  test("User is not able to update their metadata if the auth header is not present",async()=>{
-    const response = await axios.post(`${BACKEND_URL}/api/v1/user/metadata`,{
-        avatarId
-    })
+  });
+  test("User is not able to update their metadata if the auth header is not present", async () => {
+    const response = await axios.post(`${BACKEND_URL}/api/v1/user/metadata`, {
+      avatarId,
+    });
     expect(response.statusCode).toBe(403);
-  })
+  });
+});
 
+describe("User avatar information", () => {
+  let avatar;
+  let token;
+  let userId;
+  beforeAll(async () => {
+    const username = `Pratik-${Math.random()}`;
+    const password = "1234";
+    const signupRespone = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+      type: "admin",
+    });
+    userId = signupRespone.data.userId;
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+      username,
+      password,
+    });
+    token = response.data.token;
+
+    const avatarResponse = await axios.post(
+      `${BACKEND_URL}/api/v1/admin/avatar`,
+      {
+        imageUrl:
+          "https://ideogram.ai/assets/progressive-image/balanced/response/Bfvkdk12Soq7_mlr3PXJUg",
+        name: "Hercules",
+      }
+    );
+    avatarId = avatarResponse.data.avatarId;
+  });
+
+  test("Get back avatar information from a user", async () => {
+    const response = await axios.get(
+      `${BACKEND_URL}/api/v1/user/metadata/bulk?id=[${userId}]`
+    );
+    expect(response.data.avatars.length).toBe(1);
+    expect(response.data.avatars[0].userId).toBe(userId);
+  });
 });
